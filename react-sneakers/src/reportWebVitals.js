208cd72747c974794fdf93add9482014f21ef5a4
Timafei Bielawski <151946506+timafeibielawski@users.noreply.github.com>
2025-01-21 18:39:45 +0100

    Update reportWebVitals.js

--- react-sneakers/src/reportWebVitals.js
+++ react-sneakers/src/reportWebVitals.js
@@ -1,11 +1,14 @@
-const reportWebVitals = onPerfEntry => {
+const reportWebVitals = (onPerfEntry) => {
+  // Check if a callback is provided and if it's a function
   if (onPerfEntry && onPerfEntry instanceof Function) {
+    // Dynamically import the 'web-vitals' library
     import('web-vitals').then(({ getCLS, getFID, getFCP, getLCP, getTTFB }) => {
-      getCLS(onPerfEntry);
-      getFID(onPerfEntry);
-      getFCP(onPerfEntry);
-      getLCP(onPerfEntry);
-      getTTFB(onPerfEntry);
+      // Call each web-vitals function with the provided callback
+      getCLS(onPerfEntry); // Measures Cumulative Layout Shift (CLS)
+      getFID(onPerfEntry); // Measures First Input Delay (FID)
+      getFCP(onPerfEntry); // Measures First Contentful Paint (FCP)
+      getLCP(onPerfEntry); // Measures Largest Contentful Paint (LCP)
+      getTTFB(onPerfEntry); // Measures Time to First Byte (TTFB)
     });
   }
 };
