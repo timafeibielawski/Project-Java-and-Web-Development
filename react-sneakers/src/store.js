e35536f776bf9a8c9687a7685225888ee4cbe98e
Timafei Bielawski <151946506+timafeibielawski@users.noreply.github.com>
2025-01-21 18:40:17 +0100

    Update store.js

--- react-sneakers/src/store.js
+++ react-sneakers/src/store.js
@@ -1,14 +1,32 @@
-import {create} from "zustand";
+import { create } from "zustand";
 
-export const useStore = create((set ) => ({
-    user: null,
-    setUser: (user) => set({user: user}),
-    cart: [],
-    addToCart: (product) => set((state) => ({cart: [...state.cart, {product: product}]})),
-    removeFromCart: (product) => set((state) => ({cart: state.cart.filter((p) => p.product.id !== product.id)})),
-    setCart: (cart) => set({cart: cart}),
-    favorites: [],
-    addToFavorites: (product) => set((state) => ({favorites: [...state.favorites, {product: product}]})),
-    removeFromFavorites: (product) => set((state) => ({favorites: state.favorites.filter((p) => p.product.id !== product.id)})),
-    setFavorites: (favorites) => set({favorites: favorites}),
-}))
+// Zustand store definition
+export const useStore = create((set) => ({
+    // User state and setter function
+    user: null, // Stores the current user's information
+    setUser: (user) => set({ user: user }), // Updates the user state
+
+    // Cart state and functions
+    cart: [], // Stores items in the cart
+    addToCart: (product) => 
+        set((state) => ({
+            cart: [...state.cart, { product: product }], // Adds a new product to the cart
+        })),
+    removeFromCart: (product) => 
+        set((state) => ({
+            cart: state.cart.filter((p) => p.product.id !== product.id), // Removes a product from the cart by ID
+        })),
+    setCart: (cart) => set({ cart: cart }), // Sets the cart state directly
+
+    // Favorites state and functions
+    favorites: [], // Stores favorite items
+    addToFavorites: (product) => 
+        set((state) => ({
+            favorites: [...state.favorites, { product: product }], // Adds a product to favorites
+        })),
+    removeFromFavorites: (product) => 
+        set((state) => ({
+            favorites: state.favorites.filter((p) => p.product.id !== product.id), // Removes a product from favorites by ID
+        })),
+    setFavorites: (favorites) => set({ favorites: favorites }), // Sets the favorites state directly
+}));
